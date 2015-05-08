1b2ccc791d858bfb989dec435d0cf1f8857c2606
Siddharth Bharat Purohit <siddharthbharatpurohit@gmail.com>
2015-05-08 03:33:29 -0700

    modules: add remote logger module

---
+++ MAVProxy/modules/mavproxy_logger.py
@@ -0,0 +1,49 @@
+import logging
+import os
+import os.path
+import threading
+import types
+import sys
+from pymavlink import mavutil
+
+from time import sleep
+from MAVProxy.modules.lib import mp_module
+from MAVProxy.modules.lib import mp_util
+from PyCRC.CRCCCITT import CRCCCITT
+import time
+
+
+class logger(mp_module.MPModule):
+    def __init__(self, mpstate):
+        """Initialise module."""
+        super(logger, self).__init__(mpstate, "logger", "log")
+        print "Logging Started!!"
+        self.block_cnt = 0
+        self.logfile = open('/Users/Siddharth/Documents/Programming/MAVProxy/magMAVproxy/MAVProxy/modules/log.bin', 'w')
+        self.logfile.truncate()
+        self.prev_cnt = 0
+        self.download = 0
+        self.prev_download = 0
+        self.start = time.time()
+
+    def idle_task(self):
+        end = time.time()
+        if (end - self.start) >= 10:
+            print (self.download - self.prev_download)/((end - self.start)*1000), "Kb/s "
+            self.start = time.time()
+            self.prev_download = self.download
+        if 'REMOTE_LOG_DATA_BLOCK' in self.status.msgs and self.block_cnt == self.status.msgs['REMOTE_LOG_DATA_BLOCK'].block_cnt:
+            size = self.status.msgs['REMOTE_LOG_DATA_BLOCK'].block_size
+            #print ''.join(chr(e) for e in self.status.msgs['REMOTE_LOG_DATA_BLOCK'].data[:size]),
+            data = ''.join(chr(e) for e in self.status.msgs['REMOTE_LOG_DATA_BLOCK'].data[:size])
+            if(CRCCCITT().calculate(data) == self.status.msgs['REMOTE_LOG_DATA_BLOCK'].crc):
+                self.logfile.write(''.join(chr(e) for e in self.status.msgs['REMOTE_LOG_DATA_BLOCK'].data[:size]))
+                self.download+=size
+                self.block_cnt+=1
+                #print self.status.msgs['REMOTE_LOG_DATA_BLOCK']
+
+        self.master.mav.remote_log_req_block_send(self.block_cnt)
+
+def init(mpstate):
+    '''initialise module'''
+    return logger(mpstate)
